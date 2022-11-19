a51a27edae133a7a4f5a9f3ee287b96e07f4dbc1
Alejandro Rocha Cruz <alerochacruz@aol.com>
2022-11-19 20:13:47 -0300

    Add version variable

--- virtualbox-ga-install.sh
+++ virtualbox-ga-install.sh
@@ -8,6 +8,10 @@ sudo apt update ; sudo apt --yes upgrade
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 downloads_path=$(xdg-user-dir DOWNLOAD)
 
+# Set VirtualBox version
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+version="6.1.40"
+
 # Install VirtualBox Guest Additions requirements
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 sudo apt --yes install \
@@ -20,8 +24,8 @@ sudo apt --yes install \
 # https://download.virtualbox.org/virtualbox/
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 wget --directory-prefix "$downloads_path" \
-	https://download.virtualbox.org/virtualbox/6.1.38/VBoxGuestAdditions_6.1.38.iso
+	https://download.virtualbox.org/virtualbox/"$version"/VBoxGuestAdditions_"$version".iso
 sudo mkdir /media/iso
-sudo mount --options loop "$downloads_path"/VBoxGuestAdditions_6.1.38.iso /media/iso
+sudo mount --options loop "$downloads_path"/VBoxGuestAdditions_"$version".iso /media/iso
 sudo bash /media/iso/VBoxLinuxAdditions.run --nox11
 sudo umount /media/iso
